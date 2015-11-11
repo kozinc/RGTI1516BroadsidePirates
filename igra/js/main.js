435afc6981fe331e729481e901bcbdeae6a6c9ec
Kristian Žarn <kristian.zarn@gmail.com>
2015-11-12 00:32:10 +0100

    nadgradnja premikanja, locen razred za ladjico

--- igra/js/main.js
+++ igra/js/main.js
@@ -11,32 +11,25 @@ var camera;
 var scene;
 var renderer;
 
-var controls;
 var cameraMode;
 
 window.addEventListener('resize', onResize, false); // Ko spremenimo velikost brskalnika, poklicemo onResize.
 window.addEventListener('keydown', handleKeyDown, false);
-//window.addEventListener('keyup', handleKeyUp, false);
+window.addEventListener('keyup', handleKeyUp, false);
 
-window.onload = function() {
-    var keyboard = new KeyboardState();
-
+window.onload = function () {
+    // Inicializacija
     var stats = initStats(); // Prikazuj FPS-je zgoraj levo.
-
-    controls = new function() {
-        this.speedxplus = 0.0;
-        this.speedxminus = 0.0;
-
-        this.translacijaX = 0.0;
-        this.rotationSpeed = 0.01;
-        this.rotationY = 0.0;
-    };
+    var keyboard = new KeyboardState();
 
     // Ustvarimo kamero.
     ///////////////////////////////////////////////////////////////////////////////////////
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    /*camera.position.x = -30;
+     camera.position.y = 40;
+     camera.position.z = 30;
+     camera.lookAt(new THREE.Vector3(0, 0, 0));*/
     cameraMode = 2;
-
 
     // Ustvarimo 3D sceno, katera "drzi" vse objekte, osvetljevanje in kamere.
     ///////////////////////////////////////////////////////////////////////////////////////
@@ -50,214 +43,146 @@ window.onload = function() {
     // Ustvari sence. Potrebno je eksplicitno dolociti, kateri objekti mecejo in kateri prejemajo sence! In kater
     // vir svetlobe povzroca sence!
     renderer.shadowMapEnabled = true;
+    document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
-    // Ustvarimo osi in jih dodamo v sceno.
+    // Dodamo osi
     ///////////////////////////////////////////////////////////////////////////////////////
-    var axes = new THREE.AxisHelper(5);
+    var axes = new THREE.AxisHelper(10);
     scene.add(axes);
 
-    // Neka modra ravnina
+    // Dodamo morje
     ///////////////////////////////////////////////////////////////////////////////////////
-    /*var planeGeometry = new THREE.PlaneGeometry(50, 40); // Podamo velikost ravnine (x in y koordinate).
-    // Material MeshBasicMaterial se ne odziva na svetlobo!
-    var planeMaterial = new THREE.MeshLambertMaterial({color: 0x0099FF});
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.receiveShadow = true; // Vklopino prejemanje/projekcijo senc na to ravnino.
-
-    // Zarotiramo ravnino po X-osi.
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.position.x = 15;
-    plane.position.y = 0;
-    plane.position.z = 0;*/
-
-    // V sceno dodamo ravnino.
-    //scene.add(plane);
-
-    // Texture za tla (namesto modre ravnine)
-    ///////////////////////////////////////////////////////////////////////////////////////
-    var floorTexture = new THREE.ImageUtils.loadTexture('textures/checkerboard.jpg');
-    floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
-    floorTexture.repeat.set( 25, 25 );
+    var morjeTexture = new THREE.ImageUtils.loadTexture('textures/checkerboard.jpg');
+    morjeTexture.wrapS = morjeTexture.wrapT = THREE.RepeatWrapping;
+    morjeTexture.repeat.set(25, 25);
     // DoubleSide: render texture on both sides of mesh
-    var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
-    var floorGeometry = new THREE.PlaneGeometry(100, 100, 1, 1);
-    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.rotation.x = -0.5 * Math.PI;
-    floor.position.x = 15;
-    floor.position.y = 0;
-    floor.position.z = 0;
-    scene.add(floor);
+    //var morjeMaterial = new THREE.MeshLambertMaterial({map: morjeTexture});
+    var morjeMaterial = new THREE.MeshLambertMaterial({color: 0x3D64B1});
 
-    // Dodamo kocko - "ladjo"
-    ///////////////////////////////////////////////////////////////////////////////////////
-
-    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-    var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xffff00});
-    var ladja = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    ladja.castShadow = true; // Vklopimo metanje senc kocke. Pac da kocka mece sence.
-
-    ladja.position.x = -4;
-    ladja.position.y = 2;
-    ladja.position.z = 0;
-
-    //scene.add(ladja);
+    var morjeGeometry = new THREE.PlaneGeometry(100, 100, 1, 1);
+    var morje = new THREE.Mesh(morjeGeometry, morjeMaterial);
+    morje.rotation.x = -0.5 * Math.PI;
+    morje.position.x = 0;
+    morje.position.y = 0;
+    morje.position.z = 0;
+    morje.receiveShadow = true;
+    scene.add(morje);
 
     // Dodamo model ladje
     ///////////////////////////////////////////////////////////////////////////////////////
-    var group = new THREE.Group()
-    var loader = new THREE.OBJMTLLoader();
-    loader.load('models/ladja_majhna.obj', 'models/ladja_majhna.mtl', function (object) {
-        object.scale.set(1, 1, 1);
-        object.name = "LADJEVJE";
-        object.translateY(1);
-        group.add(object);
-        scene.add(group);
-    });
+    var ladja = new Ladja(scene, "ladja_majhna");
 
     // Ambientna svetloba.
     ///////////////////////////////////////////////////////////////////////////////////////
     // add subtle ambient lighting
-    var ambientLight = new THREE.AmbientLight(0x0c0c0c);
+    var ambientLight = new THREE.AmbientLight(0x2B2B2B);
     scene.add(ambientLight);
 
     // Vir svetlobe.
     ///////////////////////////////////////////////////////////////////////////////////////
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-40, 60, -10); // Lokacija izvira
-    spotLight.castShadow = true; // Vklopimo sence. Tu povemo, ali naj ta vir svetlobe naj povzroca sence.
-    scene.add(spotLight);
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(40, 50, -20);
+    directionalLight.castShadow = true;
+    directionalLight.shadowCameraNear = 2;
+    directionalLight.shadowCameraFar = 200;
+    directionalLight.shadowCameraLeft = -50;
+    directionalLight.shadowCameraRight = 50;
+    directionalLight.shadowCameraTop = 50;
+    directionalLight.shadowCameraBottom = -50;
+
+    directionalLight.distance = 0;
+    directionalLight.intensity = 1.0;
+    directionalLight.shadowMapHeight = 2048;
+    directionalLight.shadowMapWidth = 2048;
+
+    scene.add(directionalLight);
 
     // Skybox
     ///////////////////////////////////////////////////////////////////////////////////////
     var imagePrefix = "textures/skybox_";
-    var directions  = ["front", "back", "up", "down", "left", "right"];
+    var directions = ["front", "back", "up", "down", "left", "right"];
     var imageSuffix = ".jpg";
-    var skyGeometry = new THREE.CubeGeometry( 500, 500, 500 );   
-    
+    var skyGeometry = new THREE.CubeGeometry(500, 500, 500);
+
     var materialArray = [];
     for (var i = 0; i < 6; i++)
-        materialArray.push( new THREE.MeshBasicMaterial({
-            map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+        materialArray.push(new THREE.MeshBasicMaterial({
+            map: THREE.ImageUtils.loadTexture(imagePrefix + directions[i] + imageSuffix),
             side: THREE.BackSide
         }));
-    var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
-    var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
-    scene.add( skyBox );
-
-    // Poiscemo HTML objekt.
-    ///////////////////////////////////////////////////////////////////////////////////////
-    document.getElementById("WebGL-output").appendChild(renderer.domElement);
+    var skyMaterial = new THREE.MeshFaceMaterial(materialArray);
+    var skyBox = new THREE.Mesh(skyGeometry, skyMaterial);
+    //scene.add(skyBox);
 
     // GUI kontrole.
     ///////////////////////////////////////////////////////////////////////////////////////
-    var guiControls = new function() { 
+    var guiControls = new function () {
         // Spremenimo kamero.
-        this.zamenjajKamero = function(){
+        this.zamenjajKamero = function () {
             if (cameraMode == 1) {
                 cameraMode = 2;
             } else {
                 cameraMode = 1;
             }
-        }
-
-        this.dodajKocko = function () {
-
-                var cubeSize = Math.ceil((Math.random() * 3));
-                var cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-                var cubeMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
-                var cube2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-                cube2.castShadow = true;
-                cube2.name = "cube-" + scene.children.length;
-
-
-                // position the cube randomly in the scene
-                cube2.position.x = -30 + Math.round((Math.random() * floorGeometry.parameters.width));
-                cube2.position.y = Math.round((Math.random() * 5));
-                cube2.position.z = -20 + Math.round((Math.random() * floorGeometry.parameters.height));
-
-                // add the cube to the scene
-                scene.add(cube2);
-                this.numberOfObjects = scene.children.length;
-            };
-        // V konzolo izpise vse objekte. Ce zelimo izpisati samo kocke, dodamo if (lastObject instanceof THREE.Mesh)
-        // console.log(scene. getObjectByName("cube-17")
-        this.izpisiObjekte = function () {
-                console.log(scene.children);
-        }
+        };
     };
 
     var gui = new dat.GUI();
-    // Dodamo kontrole
-    gui.add(guiControls,'zamenjajKamero');
-    gui.add(guiControls,'dodajKocko');
-    gui.add(guiControls,'izpisiObjekte');
+    gui.add(guiControls, 'zamenjajKamero');
 
     // Renderiranje.
     ///////////////////////////////////////////////////////////////////////////////////////
-
     render();
     function render() {
         stats.update(); // Posodobi FPS-je.
         keyboard.update(); // Posodobi stanje tipkovnice.
 
-        // Ce ni pritisjena nobena smerna tipka, se ladjica pocasi pricne ustavljati.
-        if (!keyboard.pressed("left") && !keyboard.pressed("right") && !keyboard.pressed("up") && !keyboard.pressed("down")) {
-            if (controls.translacijaX > 0.0){
-                controls.translacijaX -= 0.005;
-            }
-            else if (controls.translacijaX < 0.0) {
-                controls.translacijaX += 0.005;
-            }
-            
-            if (controls.rotationY > 0.0){
-                controls.rotationY -= 0.0005;
-            }
-            else if (controls.rotationY < 0.0){
-                controls.rotationY += 0.0005;
-            }
+        if (keyboard.pressed("up")) {
+            ladja.moveForwardStart();
+        } else {
+            ladja.moveForwardStop();
+        }
+        if (keyboard.pressed("down")) {
+            ladja.moveBackStart();
+        } else {
+            ladja.moveBackStop();
+        }
+        if (keyboard.pressed("left")) {
+            ladja.turnLeftStart();
+        } else {
+            ladja.turnLeftStop()
+        }
+        if (keyboard.pressed("right")) {
+            ladja.turnRightStart();
+        } else {
+            ladja.turnRightStop();
         }
 
-        // rotate the cubes around its axes
-        scene.traverse(function (e) {
-            if (e instanceof THREE.Mesh && e != ladja && e != floor && e != skyBox && e instanceof THREE.Group) { //  
+        //ostane lahko samo to ce uporabim listenerje
+        ladja.updatePose();
 
-                e.rotation.x += controls.rotationSpeed;
-                e.rotation.y += controls.rotationSpeed;
-                e.rotation.z += controls.rotationSpeed;
-            }
-        });
-        // Kontrole - premikanje ladje.
-        ladja.translateX(controls.translacijaX);
-        group.translateX(controls.translacijaX);
-        //cube.translateX(-controls.translacijaX);
-        
-        // Zavrti kocko po Y-osi, ce smo jo s smernimi tipkami zavrteli.
-        ladja.rotation.y += controls.rotationY;
-        group.rotation.y += controls.rotationY;
-        //console.log(controls.rotationY) // izpisuj "hitrost" vrtenja ladje.
         transformCamera();
 
         requestAnimationFrame(render);
         renderer.render(scene, camera);
     }
 
-    // Kamera naj sledi ladji.
+    // Dolocanje lokacije kamere, glede na izbrani nacin prikaza.
     function transformCamera() {
         if (cameraMode == 1) {
             camera.position.x = -30;
             camera.position.y = 40;
             camera.position.z = 30;
-            camera.lookAt(ladja.position);
-        } else if (cameraMode == 2){
-            var relativeCameraOffset = new THREE.Vector3(-25,8,0);
-            var cameraOffset = relativeCameraOffset.applyMatrix4( group.matrixWorld );
+            camera.lookAt(ladja.model.position);
+        } else if (cameraMode == 2) {
+            var relativeCameraOffset = new THREE.Vector3(20, 10, 0);
+            var cameraOffset = relativeCameraOffset.applyMatrix4(ladja.model.matrixWorld);
 
             camera.position.x = cameraOffset.x;
             camera.position.y = cameraOffset.y;
             camera.position.z = cameraOffset.z;
-            camera.lookAt(group.position);
+            camera.lookAt(ladja.model.position);
         }
-
     }
 };
 
@@ -281,49 +206,141 @@ function onResize() {
 
 // Ko pritisnemo tipke.
 function handleKeyDown(event) {
+    var tmp_ladja = scene.getObjectByName("ladja_igralec");
     switch (event.keyCode) {
-        case 37:
-            //controls.speedzminus += 0.1;
-            // Z naslednjim stavkom povemo najvecjo hitrost obracanja ladje v levo.
-            if (controls.rotationY < 0.015){
-                controls.rotationY += 0.005;
-            }
+        case 37: // left arrow
             break;
-        case 38:
-            // Povecuj hitrost naprej.
-            if (controls.translacijaX < 0.4){
-                controls.translacijaX += 0.05;
-            }
+        case 38: // up arrow
             break;
-        case 39:
-            //controls.speedzplus += 0.1;
-            // Z naslednjim stavkom povemo najvecjo hitrost obracanja ladje v desno.
-            if (controls.rotationY > -0.015){
-                controls.rotationY -= 0.005;
-            }
+        case 39: // right arrow
             break;
-        case 40: // Nazaj
-            if (controls.translacijaX > -0.1){
-                controls.translacijaX -= 0.05;
-            }
+        case 40: // down arrow
+            break;
+    }
+}
+// Ko spustimo tipke.
+function handleKeyUp(event) {
+    var tmp_ladja = scene.getObjectByName("ladja_igralec");
+    switch (event.keyCode) {
+        case 37: // left arrow
+            break;
+        case 38: // up arrow
+            break;
+        case 39: // right arrow
+            break;
+        case 40: // down arrow
             break;
     }
 }
 
-// Ko spustimo tipke.
-/*function handleKeyUp(event) {
-    switch (event.keyCode) {
-        case 37:
-            //controls.speedzminus = 0.0;
-            break;
-        case 38: // Povecuj hitrost naprej.
-            //controls.translacijaX = 0.0;
-            break;
-        case 39:
-            //controls.speedzplus = 0.0;
-            break;
-        case 40:
-            //controls.translacijaX = 0.0;
-            break;
+function Ladja(scene, model_name) {
+    // Lastnosti
+    this.model;
+    this.vel = 0.0;
+    this.velTurn = 0.0;
+
+    this.movingFwd = false;
+    this.movingBack = false;
+    this.turningLeft = false;
+    this.turningRight = false;
+
+    // parametri premikanja
+    this.accFwd = 0.005;
+    this.accBack = 0.002;
+    this.accStop = 0.005;
+    this.velFwdMax = 0.2;
+    this.velBackMax = 0.1;
+
+    this.accTurn = 0.001;
+    this.accTurnStop = 0.0005;
+    this.velTurnMax = 0.01;
+
+    // Konstruktor
+    var group = new THREE.Group();
+    this.model = group;
+    scene.add(this.model);
+
+    var loader = new THREE.OBJMTLLoader();
+    loader.load("models/".concat(model_name).concat(".obj"), "models/".concat(model_name).concat(".mtl"),
+        function (object) {
+            object.scale.set(1, 1, 1);
+            object.name = 'ladja_igralec';
+            object.translateY(1);
+            object.children.forEach(function (tmp1) {
+                tmp1.children.forEach(function (tmp2) {
+                    tmp2.castShadow = true;
+                    tmp2.receiveShadow = true;
+                })
+            });
+            group.add(object);
+        });
+}
+// Metode
+Ladja.prototype.moveForwardStart = function () {
+    this.movingFwd = true;
+};
+Ladja.prototype.moveForwardStop = function () {
+    this.movingFwd = false;
+};
+Ladja.prototype.moveBackStart = function () {
+    this.movingBack = true;
+};
+Ladja.prototype.moveBackStop = function () {
+    this.movingBack = false;
+};
+
+Ladja.prototype.turnLeftStart = function () {
+    this.turningLeft = true;
+};
+Ladja.prototype.turnLeftStop = function () {
+    this.turningLeft = false;
+};
+Ladja.prototype.turnRightStart = function () {
+    this.turningRight = true;
+};
+Ladja.prototype.turnRightStop = function () {
+    this.turningRight = false;
+};
+
+Ladja.prototype.updatePose = function () {
+    if (this.movingFwd) {
+        if (this.vel < this.velFwdMax) {
+            this.vel += this.accFwd;
+        }
+    } else if (this.movingBack) {
+        if (this.vel > -this.velBackMax) {
+            this.vel -= this.accBack;
+        }
+    } else {
+        var sign = (this.vel >= 0) ? 1 : -1;
+        var velMag = Math.abs(this.vel);
+        if (velMag > 0) {
+            this.vel -= sign*this.accStop;
+        }
+        if (velMag < this.accStop) {
+            this.vel = 0.0;
+        }
     }
-}*/
+
+    if (this.turningLeft) {
+        if (this.velTurn < this.velTurnMax) {
+            this.velTurn += this.accTurn;
+        }
+    } else if (this.turningRight) {
+        if (this.velTurn > -this.velTurnMax) {
+            this.velTurn -= this.accTurn;
+        }
+    } else {
+        var sign = (this.velTurn >= 0) ? 1 : -1;
+        var velMag = Math.abs(this.velTurn);
+        if (velMag > 0) {
+            this.velTurn -= sign*this.accTurnStop;
+        }
+        if (velMag < this.accTurnStop) {
+            this.velTurn = 0.0;
+        }
+    }
+
+    this.model.translateX(-this.vel);
+    this.model.rotation.y += this.velTurn;
+};
