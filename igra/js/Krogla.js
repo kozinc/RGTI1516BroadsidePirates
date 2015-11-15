e368163cda8dd18343ada7d1873a0fdf8936cc6c
Kristian Žarn <kristian.zarn@gmail.com>
2015-11-15 16:16:15 +0100

    dodana eksplozija ob zadetku

--- igra/js/Krogla.js
+++ igra/js/Krogla.js
@@ -1,6 +1,6 @@
 function Krogla(parentCollisionList, power, origin, direction) {
     // Dedovanje (klici konstruktor razreda Mesh)
-    this.radius = 0.8;
+    this.radius = 0.6;
     var sphereGeometry = new THREE.SphereGeometry(this.radius, 10, 10);
     var sphereMaterial = new THREE.MeshBasicMaterial({color: 0x2E2E2E});
     THREE.Mesh.call(this, sphereGeometry, sphereMaterial);
@@ -18,6 +18,15 @@ function Krogla(parentCollisionList, power, origin, direction) {
     // Konstruktor
     this.name = "krogla_".concat(this.id.toString());
     this.position.set(origin.x, origin.y, origin.z);
+
+    // Sprite, ki se doda na sceno ob morebitnem trku
+    var spriteMap = THREE.ImageUtils.loadTexture("textures/boom2.png");
+    var spriteMaterial = new THREE.SpriteMaterial({map: spriteMap, color: 0xffffff});
+    this.explosion = new THREE.Sprite(spriteMaterial);
+    this.explosion.scale.set(10, 10, 10);
+    this.explosion.visible = false;
+    this.add(this.explosion);
+    this.timeout = 15;
 }
 // Dedovanje (nastavi metode in konstruktor)
 Krogla.prototype = Object.create(THREE.Mesh.prototype);
@@ -34,16 +43,24 @@ Krogla.prototype.update = function () {
     this.velFwd -= this.accFwd;
     this.velDown += this.accDown;
 
-    // odstrani geometrijo, ko je krogla v vodi
-    if (this.position.y < -this.radius && this.parent) {
+    // odstrani geometrijo
+    if (this.timeout < 0 && this.parent) {
         this.parent.remove(this);
         this.readyToDelete = true;
     }
 
     // preveri ce pride do kolizije
-    if (this.checkCollisions()) {
+    var intersectList = this.checkCollisions();
+    if (intersectList.length > 0) {
         this.velFwd = 0;
         this.accFwd = 0;
+        this.velDown = 0;
+        this.accDown = 0;
+
+        // prikazi eksplozijo
+        this.material.visible = false;
+        this.explosion.visible = true;
+        this.timeout -= 1;
     }
 };
 Krogla.prototype.checkCollisions = function () {
@@ -51,7 +68,6 @@ Krogla.prototype.checkCollisions = function () {
     var direction = this.direction.clone();
     direction.setY(0);
 
-    var ray = new THREE.Raycaster(origin, direction, 0, 1);
-    var intersectList = ray.intersectObjects(this.collisionObjectList, true);
-    return (intersectList.length > 0);
+    var ray = new THREE.Raycaster(origin, direction, 0, this.radius);
+    return ray.intersectObjects(this.collisionObjectList, true);
 };
