1af137ea533c398e18bf61dc668b6f6873a78cd0
Kristian Žarn <kristian.zarn@gmail.com>
2015-11-17 10:35:37 +0100

    vec nasprotnikov in posodobljeno premikanje

--- igra/js/main.js
+++ igra/js/main.js
@@ -68,17 +68,63 @@ window.onload = function () {
     ///////////////////////////////////////////////////////////////////////////////////////
     //ladja igralca
     ladja = new Ladja();
+    ladja.position.set(-162.250, 1.000, -94.557);
+    ladja.rotation.y = 1.878;
     scene.add(ladja);
 
     // Dodamo nasprotnike
     var nasprotniki = [];
 
     var path1 = [
-        new THREE.Vector3(-55.553, 0, 31.663),
-        new THREE.Vector3(-78.905, 0, -5.316)
+        new THREE.Vector3(-122.286, 0, 95.835),
+        new THREE.Vector3(-82.594, 0, 17.436),
+        new THREE.Vector3(-16.663, 0, 47.433),
+        new THREE.Vector3(9.322, 0, 98.946),
+        new THREE.Vector3(-44.747, 0, 149.477),
+        new THREE.Vector3(-111.017, 0, 128.967)
     ];
     nasprotniki.push(new LadjaNPC(path1, ladja));
     scene.add(nasprotniki[0]);
+
+    var path2 = [
+        new THREE.Vector3(144.035, 1.000, 150.561),
+        new THREE.Vector3(81.573, 1.000, 147.484),
+        new THREE.Vector3(49.792, 1.000, 85.671),
+        new THREE.Vector3(83.296, 1.000, 37.585),
+        new THREE.Vector3(128.498, 1.000, 31.236),
+        new THREE.Vector3(152.179, 1.000, 66.821),
+        new THREE.Vector3(151.942, 1.000, 116.46)
+    ];
+    nasprotniki.push(new LadjaNPC(path2, ladja));
+    scene.add(nasprotniki[1]);
+
+    var path3 = [
+        new THREE.Vector3(-44.631, 1.000, 1.849),
+        new THREE.Vector3(-16.029, 1.000, 39.528),
+        new THREE.Vector3(10.484, 1.000, 60.148),
+        new THREE.Vector3(37.552, 1.000, 43.743),
+        new THREE.Vector3(14.797, 1.000, 4.604),
+        new THREE.Vector3(25.622, 1.000, -30.887),
+        new THREE.Vector3(-34.327, 1.000, -6.573)
+    ];
+    nasprotniki.push(new LadjaNPC(path3, ladja));
+    scene.add(nasprotniki[2]);
+
+    var path4 = [
+        new THREE.Vector3(-1.251, 1.000, -116.488),
+        new THREE.Vector3(102.658, 1.000, -115.505),
+        new THREE.Vector3(141.622, 1.000, -92.990),
+        new THREE.Vector3(155.163, 1.000, -55.813),
+        new THREE.Vector3(140.654, 1.000, -22.735),
+        new THREE.Vector3(117.402, 1.000, 0.836),
+        new THREE.Vector3(138.397, 1.000, 5.729),
+        new THREE.Vector3(157.493, 1.000, -33.383),
+        new THREE.Vector3(149.401, 1.000, -115.072),
+        new THREE.Vector3(54.614, 1.000, -144.990),
+        new THREE.Vector3(-13.446, 1.000, -134.987)
+    ];
+    nasprotniki.push(new LadjaNPC(path4, ladja));
+    scene.add(nasprotniki[3]);
 
     // collision: nasprotniki - igralec
     nasprotniki.forEach(function (tmp) {
@@ -178,11 +224,10 @@ window.onload = function () {
         };
         // zapisi lokacijo ladje.
         this.printData = function () {
-            console.log(ladja.position.x.toFixed(3) + ", " +
+            console.log(
+                ladja.position.x.toFixed(3) + ", " +
                 ladja.position.y.toFixed(3) + ", " +
-                ladja.position.z.toFixed(3) + ", " +
-                ladja.rotation.y.toFixed(3) + ", " +
-                ladja.health
+                ladja.position.z.toFixed(3)
             );
         };
         this.enemyShoot = function () {
@@ -193,10 +238,10 @@ window.onload = function () {
         };
     };
 
-    var gui = new dat.GUI();
+    /*var gui = new dat.GUI();
     gui.add(guiControls, 'zamenjajKamero');
     gui.add(guiControls, 'enemyShoot');
-    gui.add(guiControls, 'printData');
+    gui.add(guiControls, 'printData');*/
 
     // Renderiranje.
     ///////////////////////////////////////////////////////////////////////////////////////
